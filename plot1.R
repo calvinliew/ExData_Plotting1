db6dcfc6e263d5bbdcf70d46e50fc65fba8022dc
calvinliew <42663288+calvinliew@users.noreply.github.com>
2018-09-28 10:28:35 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,5 +5,7 @@ subdata <- subset(data, data$Date== "1/2/2007"| data$Date=="2/2/2007")
 subdatatime <- strptime(subdata$Time, format = "%H:%M:%S" )
 subdatadate <- as.Date(subdata$Date, format = "%d/%m/%Y")
 
-hist(as.numeric(subdata$Global_active_power),xlab ="Global Active Power(kilowatts)",  ylab= "Frequency", main = "Global Active Power")
+hist(as.numeric(subdata$Global_active_power),xlab ="Global Active Power(kilowatts)",  ylab= "Frequency",col="red", main = "Global Active Power")
 
+png(filename = "plot1.png", width = 480 , height = 480)
+dev.off()
